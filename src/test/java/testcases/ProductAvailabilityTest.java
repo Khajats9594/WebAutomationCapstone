559a3ce18347d8ee7576b7e16a7adac2af4b0237
khajats <khajats@testvagrant.com>
2023-12-14 14:21:46 +0530

    Refactoring 2 base on feedBack ProductAvailabilityTest

--- src/test/java/testcases/ProductAvailabilityTest.java
+++ src/test/java/testcases/ProductAvailabilityTest.java
@@ -1,28 +1,35 @@
 package testcases;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ProductAvailabilityTest extends BaseTest {
 
     @Test
-    public void verifyProductAvailability(){
+    public void verifyProductAvailability() {
+        try {
+            //Arrange
+            String productName = "//a[contains(.,'12 Ti Xelium Skis')]";
+            String addToCart = "//button[@name='add']";
 
-        //Arrange
-        String productName = "//a[contains(.,'12 Ti Xelium Skis')]";
-        String addToCart = "//button[@name='add']";
+            //Act
+            driver.findElement(By.xpath(productName)).click();
+            String addToCatText = driver.findElement(By.xpath(addToCart)).getText();
 
-        //Act
-        driver.findElement(By.xpath(productName)).click();
-        String addToCatText = driver.findElement(By.xpath(addToCart)).getText();
-        //Assert
-        if(addToCatText.equalsIgnoreCase("Sold out")){
-            System.out.println("product is out of stock");
+            //Assert
+            if (addToCatText.equalsIgnoreCase("Sold out")) {
+                System.out.println("product is out of stock");
+                Assert.assertFalse(false);
+            } else {
+                driver.findElement(By.xpath(addToCart)).click();
+                System.out.println("product is available");
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("Element not found ");
             Assert.assertFalse(false);
-        }else {
-            driver.findElement(By.xpath(addToCart)).click();
-            System.out.println("product is available");
         }
+
     }
 }
