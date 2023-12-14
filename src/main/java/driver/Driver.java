a632c5dcf392419f07aa5a837ec1b1ccc300f90e
khajats <khajats@testvagrant.com>
2023-12-14 17:17:16 +0530

    add Driver & DriverManager class

---
+++ src/main/java/driver/Driver.java
@@ -0,0 +1,36 @@
+package driver;
+
+import enums.ConfigProperties;
+import factories.DriverFactory;
+import utiles.PropertyUtils;
+
+import java.net.MalformedURLException;
+import java.time.Duration;
+import java.util.Objects;
+
+public final class Driver {
+
+    private Driver() {}
+
+
+    public static void initDriver(String browser)  {
+
+
+        if(Objects.isNull(DriverManager.getDriver())) {
+            try {
+                DriverManager.setDriver(DriverFactory.getDriver(browser));
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Please check the capabilities of browser");
+            }
+            DriverManager.getDriver().get(PropertyUtils.get(ConfigProperties.URL));
+            DriverManager.getDriver().manage().window().maximize();
+            DriverManager.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        }
+    }
+    public static void quitDriver() {
+        if(Objects.nonNull(DriverManager.getDriver())) {
+            DriverManager.getDriver().quit();
+            DriverManager.unload();
+        }
+    }
+}
