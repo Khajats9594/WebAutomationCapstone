e68195dceff1c8759c3b0d528bf17570efd687a9
khajats <khajats@testvagrant.com>
2023-12-14 12:39:14 +0530

    Refactoring1 ProductAvailabilityTest

--- src/test/java/testcases/ProductAvailabilityTest.java
+++ src/test/java/testcases/ProductAvailabilityTest.java
@@ -8,17 +8,21 @@ import static org.testng.Assert.assertEquals;
 public class ProductAvailabilityTest extends BaseTest {
 
     @Test
-    public void verifyProductAvailability(){
+    public void verifyProductAvailability() {
         //Arrange
         String productName = "//a[contains(.,'12 Ti Xelium Skis')]";
+        String productTitle = "//h1";
         String addToChat = "//button[@name='add']";
 
         //Act
         driver.findElement(By.xpath(productName)).click();
-        String addToCatText = driver.findElement(By.xpath(addToChat)).getText();
-
-        //Assert
-        assertEquals(addToCatText,"add","product is out of stock");
-
+        boolean availability = !driver.findElements(By.xpath(productTitle)).isEmpty();
+        if (availability) {
+            String addToCatText = driver.findElement(By.xpath(addToChat)).getText();
+            System.out.println("product is available" +addToCatText);
+        }else{
+            String addToCatText = driver.findElement(By.xpath(addToChat)).getText();
+            System.out.println("product is out of stock"+addToCatText);
+        }
     }
 }
