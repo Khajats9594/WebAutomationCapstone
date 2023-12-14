2782beb7881c401d9c0783286eec5dfd141b3d6b
khajats <khajats@testvagrant.com>
2023-12-14 18:53:21 +0530

    refactoring verifyAddToCartForAvailableProduct

--- src/test/java/testcases/AddToCartForAvailableProductTest.java
+++ src/test/java/testcases/AddToCartForAvailableProductTest.java
@@ -16,21 +16,22 @@ public class AddToCartForAvailableProductTest extends BaseTest{
     private static final Logger logger = LogManager.getLogger(AddToCartForAvailableProductTest.class);
 
     @Test(description = "verify the add to cart for availability product")
-    public void verifyAddToCartForAvailableProduct(){
+    public void verifyAddToCartForAvailableProduct() throws InterruptedException {
 
         //Arrange
         String productName = "//a[contains(.,'15mm Combo Wrench')]";
         String addToCart = "//button[@name='add']";
-        String cartMessageText = "//div[@id='cart-notification']";
+        String cartMessageText = "//div[@id='cart-notification']/div/h2";
         String viewCartPage = "//a[@id='cart-notification-button']";
         String itemOnCartCount = "//input[@aria-label='Quantity for 15mm Combo Wrench']";
 
         //Act
         driver.findElement(By.xpath(productName)).click();
         driver.findElement(By.xpath(addToCart)).click();
-        String addToCartMessage = new WebDriverWait(driver, Duration.ofSeconds(20))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(cartMessageText)))
-                .getAttribute("aria-label");
+        String addToCartMessage = new WebDriverWait(driver, Duration.ofSeconds(30))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(cartMessageText)))
+                .getText();
+        System.out.println(addToCartMessage+": message");
 
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.elementToBeClickable(By.xpath(viewCartPage)))
