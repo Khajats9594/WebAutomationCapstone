f91ac6cdbd28dafa6fc52d173ddb801f9facfeee
khajats <khajats@testvagrant.com>
2023-12-14 20:36:54 +0530

    add ExplicitWait in FrameworkConstants

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -1,16 +1,15 @@
 package constants;
-
-import lombok.Data;
-import lombok.Getter;
-
-
 public final class FrameworkConstants {
 
     private FrameworkConstants() {}
 
+    private static final int EXPLICITWAIT = 10;
     private static final String RESOURCESPATH = System.getProperty("user.dir")+"/src/test/resources";
     private static final String CONFIGFILEPATH = RESOURCESPATH+"/config-files/config.properties";
     public static String getConfigFilePath() {
         return CONFIGFILEPATH;
+    }
+    public static int getExplicitwait() {
+        return EXPLICITWAIT;
     }
 }
