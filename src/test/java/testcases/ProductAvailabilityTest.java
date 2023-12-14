676b362fd76af0ea5250ff3f5cbaff2716667322
khajats <khajats@testvagrant.com>
2023-12-14 14:40:26 +0530

    refactoring..

--- src/test/java/testcases/ProductAvailabilityTest.java
+++ src/test/java/testcases/ProductAvailabilityTest.java
@@ -1,16 +1,20 @@
 package testcases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ProductAvailabilityTest extends BaseTest {
+    private static final Logger logger = LogManager.getLogger(ProductAvailabilityTest.class);
 
     @Test
     public void verifyProductAvailability() {
         try {
             //Arrange
+            logger.info("");
             String productName = "//a[contains(.,'12 Ti Xelium Skis')]";
             String addToCart = "//button[@name='add']";
 
@@ -20,15 +24,15 @@ public class ProductAvailabilityTest extends BaseTest {
 
             //Assert
             if (addToCatText.equalsIgnoreCase("Sold out")) {
-                System.out.println("product is out of stock");
-                Assert.fail("product is out of stock");
+                logger.error("Product is out of stock");
+                Assert.fail("Product is out of stock, ending test.");
             } else {
+                logger.info("Product is available, proceeding with the test.");
                 driver.findElement(By.xpath(addToCart)).click();
-                System.out.println("product is available");
             }
         } catch (NoSuchElementException e) {
-            System.out.println("Element not found ");
-            Assert.fail("Element not found ");
+            logger.error("Element not found: " + e.getMessage());
+            Assert.fail("Element not found, ending test.");
         }
 
     }
