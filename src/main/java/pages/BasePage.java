054bdc1d7b96405772db8357d440ae3e06e103cb
khajats <khajats@testvagrant.com>
2023-12-15 13:39:47 +0530

    added logs in BasePage.java

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,6 +2,8 @@ package pages;
 
 import enums.WaitStrategy;
 import factories.ExplicitWaitFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -9,25 +11,29 @@ public class BasePage {
     protected BasePage() {
 
     }
-
+    private static final Logger logger = LogManager.getLogger(BasePage.class);
     protected void sendKey(By by, String value, WaitStrategy waitingStrategy) {
         WebElement element = ExplicitWaitFactory.performExplicitWait(waitingStrategy, by);
         element.sendKeys(value);
+        logger.info("{} value enter into the element : {}",value,element);
     }
 
     protected void click(By by, WaitStrategy waitingStrategy) {
         WebElement element = ExplicitWaitFactory.performExplicitWait(waitingStrategy, by);
         element.click();
+        logger.info("{} is clicked ",element);
     }
 
     protected String getText(By by,WaitStrategy waitStrategy) {
-        return ExplicitWaitFactory.performExplicitWait(waitStrategy,by)
-                .getText();
+        String textOfElement = ExplicitWaitFactory.performExplicitWait(waitStrategy, by).getText();
+        logger.info("{} is fetched",textOfElement);
+        return textOfElement;
     }
 
     protected String getValueAttribute(By by, WaitStrategy waitStrategy){
-        return ExplicitWaitFactory.performExplicitWait(waitStrategy,by)
-                .getAttribute("value");
+        String value = ExplicitWaitFactory.performExplicitWait(waitStrategy, by).getAttribute("value");
+        logger.info("value fetch from value attribute is {}",value);
+        return value;
     }
 
     protected boolean isPresent(By by, WaitStrategy waitStrategy){
