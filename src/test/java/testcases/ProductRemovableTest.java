f7dd0d1bf94b78a06dcff14f93ce9c1b1fca0575
khajats <khajats@testvagrant.com>
2023-12-15 11:47:09 +0530

    add ProductRemovableTest

---
+++ src/test/java/testcases/ProductRemovableTest.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LandingPage;
+
+public class ProductRemovableTest extends BaseTest{
+
+    private static final Logger logger = LogManager.getLogger(ProductRemovableTest.class);
+
+    @Test
+    public void productRemovableTest(){
+        //Arrange
+        String productName = "15mm Combo Wrench";
+        logger.info("Product detail page is testing using productName is {}",productName);
+
+        //Act
+        boolean verifiedProductRemovable = LandingPage.getInstance()
+                .navigateToProductPage(productName)
+                .addToCart()
+                .clickViewCartPageBtn()
+                .verifyProductRemovable(productName);
+        logger.info("Product is removed? {}",verifiedProductRemovable);
+
+        //Assert
+        Assert.assertTrue(verifiedProductRemovable, "Given product is not removed");
+    }
+}
