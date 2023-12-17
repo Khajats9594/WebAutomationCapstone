76b788b9f794cc417c0b9f77fc2076b64c785751
khajats <khajats@testvagrant.com>
2023-12-17 13:14:44 +0530

    Refactored the ExplicityWait to 20

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -3,7 +3,7 @@ public final class FrameworkConstants {
 
     private FrameworkConstants() {}
 
-    private static final int EXPLICITWAIT = 10;
+    private static final int EXPLICITWAIT = 20;
     private static final String RESOURCESPATH = System.getProperty("user.dir")+"/src/test/resources";
     private static final String CONFIGFILEPATH = RESOURCESPATH+"/config-files/config1.properties";
     public static String getConfigFilePath() {
