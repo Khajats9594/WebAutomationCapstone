db910d0c2e66c7e1f037c89d22519616a270a552
khajats <khajats@testvagrant.com>
2023-12-19 13:20:38 +0530

    added file path of userData json file in FrameworkConstants.java

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -6,10 +6,15 @@ public final class FrameworkConstants {
     private static final int EXPLICITWAIT = 20;
     private static final String RESOURCESPATH = System.getProperty("user.dir")+"/src/test/resources";
     private static final String CONFIGFILEPATH = RESOURCESPATH+"/config-files/config1.properties";
+    private static final String LOGINDATA = System.getProperty("user.dir")+"/src/test/resources/data_set/userData.json";
+
     public static String getConfigFilePath() {
         return CONFIGFILEPATH;
     }
     public static int getExplicitwait() {
         return EXPLICITWAIT;
+    }
+    public static String getLoginDataFilePath(){
+        return LOGINDATA;
     }
 }
