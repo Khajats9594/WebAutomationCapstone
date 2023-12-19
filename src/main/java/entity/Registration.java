a0288b2f601503f6e74f02056cdd307013325a5c
khajats <khajats@testvagrant.com>
2023-12-19 17:13:13 +0530

    added Registration.java entity

---
+++ src/main/java/entity/Registration.java
@@ -0,0 +1,22 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(setterPrefix = "set")
+public class Registration {
+    @JsonProperty("firstName")
+    private String firstName;
+    @JsonProperty("lastName")
+    private String lastName;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
+}
